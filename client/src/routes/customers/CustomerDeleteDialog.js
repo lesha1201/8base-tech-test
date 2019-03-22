923a764932db27f83f9bbce46f18a47c9edab4b4
Alexey Ryabov <lesha_12_01@mail.ru>
2019-03-22 14:29:31 +0300

    [client] add scripts for formatting; run formatter (prettier)

--- client/src/routes/customers/CustomerDeleteDialog.js
+++ client/src/routes/customers/CustomerDeleteDialog.js
@@ -12,8 +12,8 @@ const CUSTOMER_DELETE_DIALOG_ID = 'CUSTOMER_DELETE_DIALOG_ID';
 class CustomerDeleteDialog extends React.Component {
   static contextType = ModalContext;
 
-  createOnSubmit = R.memoize((id) => async () => {
-    await this.props.customerDelete({ variables: { id }});
+  createOnSubmit = R.memoize(id => async () => {
+    await this.props.customerDelete({ variables: { id } });
 
     this.context.closeModal(CUSTOMER_DELETE_DIALOG_ID);
   });
@@ -23,30 +23,28 @@ class CustomerDeleteDialog extends React.Component {
   };
 
   renderFormContent = ({ handleSubmit, invalid, submitting }) => (
-    <form onSubmit={ handleSubmit }>
-      <Dialog.Header title="Delete Customer" onClose={ this.onClose } />
-      <Dialog.Body scrollable>
-        Are you really want to delete customer?
-      </Dialog.Body>
+    <form onSubmit={handleSubmit}>
+      <Dialog.Header title="Delete Customer" onClose={this.onClose} />
+      <Dialog.Body scrollable>Are you really want to delete customer?</Dialog.Body>
       <Dialog.Footer>
-        <Button color="neutral" variant="outlined" disabled={ submitting } onClick={ this.onClose }>Cancel</Button>
-        <Button color="danger" type="submit" disabled={ invalid } loading={ submitting }>Delete Customer</Button>
+        <Button color="neutral" variant="outlined" disabled={submitting} onClick={this.onClose}>
+          Cancel
+        </Button>
+        <Button color="danger" type="submit" disabled={invalid} loading={submitting}>
+          Delete Customer
+        </Button>
       </Dialog.Footer>
     </form>
   );
 
   renderContent = ({ args }) => {
-    return (
-      <Form onSubmit={ this.createOnSubmit(args.id) }>
-        { this.renderFormContent }
-      </Form>
-    );
+    return <Form onSubmit={this.createOnSubmit(args.id)}>{this.renderFormContent}</Form>;
   };
 
   render() {
     return (
-      <Dialog id={ CUSTOMER_DELETE_DIALOG_ID } size="sm">
-        { this.renderContent }
+      <Dialog id={CUSTOMER_DELETE_DIALOG_ID} size="sm">
+        {this.renderContent}
       </Dialog>
     );
   }
@@ -57,7 +55,7 @@ CustomerDeleteDialog = graphql(sharedGraphQL.CUSTOMER_DELETE_MUTATION, {
   options: {
     refetchQueries: ['CustomersList'],
     context: {
-      [TOAST_SUCCESS_MESSAGE]: 'Customer successfuly deleted'
+      [TOAST_SUCCESS_MESSAGE]: 'Customer successfuly deleted',
     },
   },
 })(CustomerDeleteDialog);
--- client/src/routes/properties/PropertyEditDialog.js
+++ client/src/routes/properties/PropertyEditDialog.js
@@ -13,8 +13,8 @@ const PROPERTY_EDIT_DIALOG_ID = 'PROPERTY_EDIT_DIALOG_ID';
 class PropertyEditDialog extends React.Component {
   static contextType = ModalContext;
 
-  createOnSubmit = R.memoize((id) => async (data) => {
-    await this.props.propertyUpdate({ variables: { data: { ...data, id } }});
+  createOnSubmit = R.memoize(id => async data => {
+    await this.props.propertyUpdate({ variables: { data: { ...data, id } } });
 
     this.context.closeModal(PROPERTY_EDIT_DIALOG_ID);
   });
@@ -24,57 +24,66 @@ class PropertyEditDialog extends React.Component {
   };
 
   renderFormContent = ({ handleSubmit, invalid, submitting, pristine }) => (
-    <form onSubmit={ handleSubmit }>
-      <Dialog.Header title="Edit Property" onClose={ this.onClose } />
+    <form onSubmit={handleSubmit}>
+      <Dialog.Header title="Edit Property" onClose={this.onClose} />
       <Dialog.Body scrollable>
         <Grid.Layout gap="sm" stretch>
           {
             <Grid.Box>
-              <Field name="pictures" label="Pictures" component={ FileInputField } maxFiles={ 20 } />
+              <Field name="pictures" label="Pictures" component={FileInputField} maxFiles={20} />
             </Grid.Box>
           }
           <Grid.Box>
-            <Field name="title" label="Title" type="text" component={ InputField } />
+            <Field name="title" label="Title" type="text" component={InputField} />
           </Grid.Box>
           <Grid.Box>
-            <Field name="description" label="Description" type="text" component={ InputField } />
+            <Field name="description" label="Description" type="text" component={InputField} />
           </Grid.Box>
           <Grid.Box>
-            <Field name="bedrooms" label="Bedrooms" type="text" component={ InputField } />
+            <Field name="bedrooms" label="Bedrooms" type="text" component={InputField} />
           </Grid.Box>
           <Grid.Box>
-            <Field name="sqFootage" label="Sq Footage" type="text" component={ InputField } />
+            <Field name="sqFootage" label="Sq Footage" type="text" component={InputField} />
           </Grid.Box>
           <Grid.Box>
-            <Field name="bathrooms" label="Bathrooms" type="text" component={ InputField } />
+            <Field name="bathrooms" label="Bathrooms" type="text" component={InputField} />
           </Grid.Box>
           <Grid.Box>
-            <Field name="garage" label="Garage" component={ CheckboxField } />
+            <Field name="garage" label="Garage" component={CheckboxField} />
           </Grid.Box>
           <Grid.Box>
-            <Field name="pool" label="Pool" component={ CheckboxField } />
+            <Field name="pool" label="Pool" component={CheckboxField} />
           </Grid.Box>
         </Grid.Layout>
       </Dialog.Body>
       <Dialog.Footer>
-        <Button color="neutral" variant="outlined" disabled={ submitting } onClick={ this.onClose }>Cancel</Button>
-        <Button color="primary" type="submit" disabled={ pristine || invalid } loading={ submitting }>Update Property</Button>
+        <Button color="neutral" variant="outlined" disabled={submitting} onClick={this.onClose}>
+          Cancel
+        </Button>
+        <Button color="primary" type="submit" disabled={pristine || invalid} loading={submitting}>
+          Update Property
+        </Button>
       </Dialog.Footer>
     </form>
   );
 
   renderForm = ({ args }) => {
     return (
-      <Form type="UPDATE" tableSchemaName="Properties" onSubmit={ this.createOnSubmit(args.initialValues.id) } initialValues={ args.initialValues }>
-        { this.renderFormContent }
+      <Form
+        type="UPDATE"
+        tableSchemaName="Properties"
+        onSubmit={this.createOnSubmit(args.initialValues.id)}
+        initialValues={args.initialValues}
+      >
+        {this.renderFormContent}
       </Form>
     );
   };
 
   render() {
     return (
-      <Dialog id={ PROPERTY_EDIT_DIALOG_ID } size="sm">
-        { this.renderForm }
+      <Dialog id={PROPERTY_EDIT_DIALOG_ID} size="sm">
+        {this.renderForm}
       </Dialog>
     );
   }
@@ -85,7 +94,7 @@ PropertyEditDialog = graphql(sharedGraphQL.PROPERTY_UPDATE_MUTATION, {
   options: {
     refetchQueries: ['PropertiesList'],
     context: {
-      [TOAST_SUCCESS_MESSAGE]: 'Property successfuly updated'
+      [TOAST_SUCCESS_MESSAGE]: 'Property successfuly updated',
     },
   },
 })(PropertyEditDialog);
