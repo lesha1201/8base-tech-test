7b56e6d72f0e09d2a7a32654038977197a8e99ea
Alexey Ryabov <lesha_12_01@mail.ru>
2019-03-24 19:21:21 +0300

    [client] add new graphql tags for Clients, Orders, Products

--- client/src/shared/graphql/index.js
+++ client/src/shared/graphql/index.js
@@ -38,9 +38,37 @@ export const CLIENT_QUERY = gql`
   }
 `;
 
+export const CLIENT_ORDERS_LIST_QUERY = gql`
+  query Client($id: ID!) {
+    client(id: $id) {
+      id
+      orders {
+        items {
+          id
+          address
+          deliveryDt
+          comment
+          status
+          orderItems {
+            items {
+              id
+              product {
+                id
+                name
+                price
+              }
+              quantity
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const CLIENTS_LIST_QUERY = gql`
-  query ClientsList {
-    clientsList {
+  query ClientsList($orderBy: [ClientOrderBy], $skip: Int, $first: Int) {
+    clientsList(orderBy: $orderBy, skip: $skip, first: $first) {
       items {
         id
         firstName
@@ -49,6 +77,141 @@ export const CLIENTS_LIST_QUERY = gql`
         phone
         birthday
       }
+    }
+  }
+`;
+
+/* Orders */
+export const ORDER_CREATE_MUTATION = gql`
+  mutation OrderCreate($data: OrderCreateInput!) {
+    orderCreate(data: $data) {
+      id
+    }
+  }
+`;
+
+export const ORDER_DELETE_MUTATION = gql`
+  mutation OrderDelete($id: ID!) {
+    orderDelete(data: { id: $id }) {
+      success
+    }
+  }
+`;
+
+export const ORDER_UPDATE_MUTATION = gql`
+  mutation OrderUpdate($data: OrderUpdateInput!) {
+    orderUpdate(data: $data) {
+      id
+    }
+  }
+`;
+
+export const ORDER_QUERY = gql`
+  query Order($id: ID!) {
+    order(id: $id) {
+      id
+      address
+      deliveryDt
+      comment
+      status
+      client {
+        id
+        firstName
+      }
+    }
+  }
+`;
+
+export const ORDER_PRODUCTS_LIST_QUERY = gql`
+  query Order($id: ID!) {
+    order(id: $id) {
+      id
+      orderItems {
+        items {
+          id
+          product {
+            id
+            name
+            price
+            picture {
+              downloadUrl
+            }
+          }
+          quantity
+        }
+      }
+    }
+  }
+`;
+
+export const ORDERS_LIST_QUERY = gql`
+  query OrdersList($orderBy: [OrderOrderBy], $skip: Int, $first: Int) {
+    ordersList(orderBy: $orderBy, skip: $skip, first: $first) {
+      items {
+        id
+        client {
+          id
+          firstName
+        }
+        address
+        deliveryDt
+        comment
+        status
+        orderItems {
+          items {
+            id
+            product {
+              id
+              price
+            }
+            quantity
+          }
+        }
+      }
+      count
+    }
+  }
+`;
+
+/* Products */
+export const PRODUCT_CREATE_MUTATION = gql`
+  mutation ProductCreate($data: ProductCreateInput!) {
+    productCreate(data: $data) {
+      id
+    }
+  }
+`;
+
+export const PRODUCT_DELETE_MUTATION = gql`
+  mutation ProductDelete($id: ID!) {
+    productDelete(data: { id: $id }) {
+      success
+    }
+  }
+`;
+
+export const PRODUCT_UPDATE_MUTATION = gql`
+  mutation ProductUpdate($data: ProductUpdateInput!) {
+    productUpdate(data: $data) {
+      id
+    }
+  }
+`;
+
+export const PRODUCTS_LIST_QUERY = gql`
+  query ProductsList($orderBy: [ProductOrderBy], $skip: Int, $first: Int) {
+    productsList(orderBy: $orderBy, skip: $skip, first: $first) {
+      items {
+        id
+        picture {
+          id
+          downloadUrl
+        }
+        name
+        description
+        price
+      }
+      count
     }
   }
 `;
